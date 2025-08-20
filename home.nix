8a8b2b6b32066e8e56c88ef867d7b9268eb1d48d
clapo-bit <jamal.siddique.work@gmail.com>
2025-08-20 13:18:41 +0100

    NixOS config

--- home.nix
+++ home.nix
@@ -1,7 +1,6 @@
 {pkgs, ... }:
 {
  
-
  #Home manager core details
  home.username = "jamal";
  home.stateVersion = "23.05";
@@ -11,9 +10,20 @@
     enable = true;
     initContent = "
       # Rebuild system
-      alias nixsys='sudo nixos-rebuild switch --flake ~/nixos'
+      alias nixsys='sudo nixos-rebuild switch --flake path:$HOME/nixos --impure'
       # Clean builds to last 3
       alias nixcl3='sudo nix-env --delete-generations +3 && sudo nix-collect-garbage -d' 
+      # Github backup alias
+      alias nixbackup='
+        cd ~/nixos
+	if ! git diff-index --quiet HEAD--; then
+	  git add .
+	  git commit -m \"NixOS config $(date + %d-%m-%Y_%H-%M-%S)\"
+	  git push origin main
+	else
+	  echo \"No changes to commit\"
+	fi'
+
       ";
   };
  
